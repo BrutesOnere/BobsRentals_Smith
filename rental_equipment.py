27fc6ae463fa39ada4544108443a3a40d05fce3b
Michael Smith <mssmith5@cincinnatistate.edu>
2026-08-02 19:04:18 -0400

    Implement RentalEquipment class with attributes
    was having serious trouble figuring out how to commit, but i think i have it figured out. and made sure i used encapsulated attributes.

---
+++ rental_equipment.py
@@ -0,0 +1,24 @@
+# rental_equipment.py
+	class RentalEquipment:
+		def __init__(self,name,equipment_type, hourly_rate, daily_rate, weekly_rate):
+			# Encapsulated attributes
+			self._name = name
+			self._equipment_type
+			self._hourly_rate
+			self._daily_rate
+			self._weekly_rate
+
+		def get_name(self):
+			return self._name
+
+		def get_equipment_type(self):
+			return self._equipment_type
+	
+		def get_hourly_rate(self):
+			return self._hourly_rate
+
+		def get_daily_rate(self):
+			return self._daily_rate
+
+		def getweekly_rate(self):
+			return self._weekly_rate
